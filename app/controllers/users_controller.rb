cd59e0e5aed44c27d3a6df6949f2a4cdf3494014
Timotius Vincent <timotius.vincent99@gmail.com>
2021-05-14 20:20:19 -0700

    added lost files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,71 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    @user.avatar.attach(params[:avatar])
+    if @user.save
+      render json: {
+        status: 'SUCCESS',
+        message: 'User saved',
+        data: @user,
+        avatar: url_for(@user.avatar)},
+        status: :created
+    else
+      render json: {
+        status: 'ERROR',
+        message: 'User not saved',
+        data: @user.errors},
+        status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find_by(access_token: params[:access_token]) if params[:access_token].present?
+    @user = User.find(params[:id]) if params[:id].present?
+    @avatar = @user.avatar
+    if @user.nil?
+      render json: {
+        status: 'NOT FOUND',
+        message: 'User Not Found',
+        data: @user},
+        status: :not_found
+    end
+    render json: {
+      status: 'SUCCESS',
+      message: 'User Found',
+      data: @user,
+      avatar: url_for(@user.avatar)},
+      status: :ok
+  end
+
+  def update
+    @user = User.find_by(access_token: params[:access_token]) if params[:access_token].present?
+    @user = User.find(params[:id]) if params[:id].present?
+    if params.has_key?(:avatar)
+      if @user.avatar.attached?
+        @user.avatar.purge
+      end
+      @user.avatar.attach(params[:avatar])
+      @user.save!
+    end
+    if @user.update(user_params)
+      render json: {
+        status: 'SUCCESS',
+        message: 'User updated',
+        data: @user,
+        avatar: url_for(@user.avatar)},
+        status: :ok
+    else
+      render json: {
+        status: 'ERROR',
+        message: 'User not updated',
+        data: @user.errors},
+        status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:access_token, :name, :gender, :fun_fact, :email)
+  end
+end
--- app/models/user_active_event.rb
+++ app/models/user_active_event.rb
@@ -1,3 +1,6 @@
 class UserActiveEvent < ApplicationRecord
+  has_many :users
+  has_many :events
+
   scope :filter_by_user, -> (user_id) { where user_id: user_id }
 end
