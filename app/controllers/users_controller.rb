757394bbf1a16fae3cd892c378b3ab444ce8dbbb
Timotius Vincent <timotius.vincent99@gmail.com>
2021-05-15 22:28:42 -0700

    optional avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    @user.avatar.attach(params[:avatar])
+    @user.avatar.attach(params[:avatar]) if params[:avatar].present?
     if @user.save
       render json: {
         status: 'SUCCESS',
